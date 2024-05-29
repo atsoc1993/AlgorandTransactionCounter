9a979ea8077838b499f195f8538cc9b8d4cad3cb
atsoc1993 <144640214+atsoc1993@users.noreply.github.com>
2024-05-29 11:27:56 -0400

    Create ProofOfConcept.py

---
+++ ProofOfConcept.py
@@ -0,0 +1,75 @@
+single_transaction = {
+    'confirmed-round': 39282074,
+    'pool-error': '',
+    'txn': {
+        'sig': 'BLxCLODBoe/WhKU9N9SQqJ+uuIr0UMb7jEU30jx+coeWnBqGvJy0SnIiGVGfNrhEpkOhiMgS6aS6qqyHJBmbDg==',
+        'txn': {
+            'apaa': ['dmVyaWZ5VkFB', 'AQAAAAQNAFtuB4zYLPVE7oA3NAmY1x4TPCydRHUHZdjeXkKcm4rYWhXxTXihqxmbhqUvEiyrVxw1XIsq/UobogWfMO7bWO4BAQ6anvhGBcTZ6vgI3LOlhHGKK2HY9PrhaKrrBZGdSolUYXMUamh2xGb+0qGqpDeTa6Us74RaRlp/dzHspQ9MOYwBAoaGjLfy31GUGb77FAd48aTAc5rHHA+negYX0bvYBINEGDlRcDFnzv7YfMOc0CxxxFGXX1Rs2NwGWddP/ncLLm0AA5M2YcDvOH+gA8tsAFop8S8Ki86F3FhD27JAVU9CWmUwGPmuyLoADZDgG085wwA5nAnDFCwGrSUFOXlpPE7fnjYBBCvz8jZAJ5mnO2+XirFVKEioliNrVA2GCxlpP32u3hPpJn/eTGaau6FL2cse3k6ZqO50so0F66fXTqIpgnZz1tUABue0yfpN/oS8hYGbjqPml6bseEe6mmMy2KqpUgvB4cf2LiL0loZHkx47Qg8Pa1shayZIVh0F9tvG5RoPeLKTb6MBCXNI0gr9g6+bEHDJd6H7USx9CiXFHZeAKbkMCMuO2uPvZdSvsoOSxsm8GC+m691iv4lMDLdVb4wy9z4gUaKxmDEBCmPABlenUJ41VYoG6vv/MfKdTXb6fa6Ntimu0FT9xtmvRpjW9YaVLGQGTRB0AP8aad5qqrK0KcgHc8GvWlHNVpMBC0vaDRdc/i13RxSmgQtakLXMznelqYtdoo9wr1SnzvKre9XJ4Oi5xqRolIS+Gjf24U5FLj69UWnbKuLBpRm2RHQADU6lUVlbUsr+TWtC2TVfMeLInzntuqv54XC6qmp60CHgKZOFYj/XiB0TeI2wUs4YPfU2S5sigs/RgkqbS86UXTEADjA3O/toDrUv3lIdNAIpq+mC/a6jb1m3sQvAhsVQLIkdN10wUBBqqu67iRig2B+CdCBP47Z88JOdJGKEO7t8xSUBD33odxWFfcVahORpPmW3CFQ567KQ7intAR7Z0fWVJvqPVlLHEYXpreOLOHGcF0S1TG+AK+PDl3S3Aidfc4fZ5TUBElLA41XPgElDgpn/qK0remGQXj4tp4FbGfLcpEXBpBozFkfvxrbtMyhnZ9e4uR2ecKXzPDYgEo+GTcqNSMUx4acAZldGIQAAAAAAGuEB+u2sWFHjK5sjtflBGowrrEquPtTde4Ed0acupKpxAAAAAAOOUkgBQVVXVgAAAAAACJhw5wAAJxAlTJIaVyeoZ2vkAOhnrnFNGAjfyA=='],
+            'apat': ['BCC2AU6DNY4ICK4O4SLMMEFOZDFQYAYVT25V5TTBVXCRX3BSKPYX6E6R6Y', 'XJC32PG73M4VIWAAZQZX6LRHPDAX2DMGVGQJJZSWJUZ5LECEVFMGJENX2M'],
+            'apid': 842125965,
+            'fee': 4000,
+            'fv': 39282072,
+            'gen': 'mainnet-v1.0',
+            'gh': 'wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=',
+            'grp': 'im+7j9SXQ0bs0VftWdnDLqaCtVDdU563vE3+2TF2fIM=',
+            'lv': 39283072,
+            'snd': 'C3PRI7UN4CGR64ASB45JBAWH6CPN75V3LWF4VZL7W6F5OOAF733JCTM6OA',
+            'type': 'appl'
+        }
+    },
+    'inner-txns': [
+        {'application-index': 1890072557, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072558, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072559, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072560, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072561, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072562, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072563, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072564, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072565, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072566, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072567, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072568, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072569, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072570, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072571, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072572, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072573, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072574, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072575, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072576, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072577, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072578, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072579, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072580, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072581, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072582, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072583, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}},
+        {'application-index': 1890072584, 'pool-error': '', 'txn': {'txn': {'apan': 5, 'apap': 'BoEB', 'apsu': 'BoEB', 'fv': 39281988, 'lv': 39282981, 'snd': 'ZOMFBEZ53Z7Z3XJSIR43ZMEX6VIJDK2IYK4ZAEJ7IUNZJHXHRLXAYX7FOI', 'type': 'appl'}}}
+    ],
+    'logs': [
+        'U1RPUkVAAAAAAAAAAAgAAAAAZGo3HAAAAAAABn9IAAAAAAKJuCAAAAAAAAD6AP////gAAAAAAoqrcgAAAAAAANr6AAAAAGZXRiEAAAAAZldGIQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==',
+        'U1RPUkVAAAAAAAAAAAkAAAAAZZDlkgAAAAAACQ3+AAAAAAOJd2UAAAAAAAFfJf////gAAAAAA4crQgAAAAAAAWQJAAAAAGZXRiEAAAAAZldGIQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=='
+    ],
+    'pool-error': '',
+    'txn': {
+        'sig': 'aX1BE+3/aBQQGwYlHIo3x0sxEFEQeXtjm8JL23jjPUYS/+g51snB3pBDJRSbfGhnZ2AKTiG44yVTuknnp1hDCQ==',
+        'txn': {
+            'apaa': ['c3RvcmU=', 'JUySGlcnqGdr5ADoZ65xTRgI38g=', 'AAAAAGRqNxwIAAAAAGWQ5ZIJ', 'AgBVAAu/KOmoQaHMeI9qNhsXygctDqMJih5d8cOSLQZxlXn/AAAAAAKJuCAAAAAAAAD6AP////gAAAAAZldGIQAAAABmV0YhAAAAAAKKq3IAAAAAAADa+gq9Bb+6f85ycsyZ1FMi8hkODr4Tia/essab9EnX6Q6TCRm5S640sVbbTIykHuwY4mEYJtTqVPBU2RBM43xmDlq2MSWlx9dYx076rdeBPff6nMudrr7ghRhacmATRObdlQt6XmW0i8S1tKUWGcezZvb6tc3AX/5Q3ts7TTIbRMWZplOpeMyCmvqlqrjmRwi5H9en4reolY1SLiiLJPGtihxz0ijrMq7FKvA7e0/mAgyzoKT1dP5ETpJnHLwzkRtqXo2sFRFLs3Nb/QBVAO/3RGR14hhRdWbqmecqSr7C4b2EmLQ7fYMx4p3LBZOJAAAAAAOJd2UAAAAAAAFfJf////gAAAAAZldGIQAAAABmV0YhAAAAAAOHK0IAAAAAAAFkCQqSjzkkP/nPHJi0JWVXFOPgma8SZrJ/uskAmtyblJqyA/ALM7rov4otLxpZg3O7mwa/gL4jAyiC5QzuPX7+pIiZyCp4xqGc8L272d+UON74BW6xoztlTGfe0SCaw8SwP/FISiUpU9j5V0HLpmcpQ8CCKpGm6ssNka84cqgWhHylw7X0AoB60eLLExXhbk3ajNEPtxMe9JvABj4gixb6y/lt+8wr7g8IjiOzlzInQHjeipmRDaFETpJnHLwzkRtqXo2sFRFLs3Nb/Q==', 'AAAAAAAATiA='],
+            'apas': [1684682524, 1703994770],
+            'apid': 1257258120,
+            'fee': 30000,
+            'fv': 39281988,
+            'gen': 'mainnet-v1.0',
+            'gh': 'wGHE2Pwdvd7S12BL5FaOP20EGYesN73ktiC1qzkkit8=',
+            'grp': 'im+7j9SXQ0bs0VftWdnDLqaCtVDdU563vE3+2TF2fIM=',
+            'lv': 39282981,
+            'snd': 'C3PRI7UN4CGR64ASB45JBAWH6CPN75V3LWF4VZL7W6F5OOAF733JCTM6OA',
+            'type': 'appl'
+        }
+    }
+}
+
+inner_txns = data.get('inner-txns', [])
+total_txns = len(inner_txns) + 1 #The + 1 is to count the main transaction
+print(total_txns) #Output: 29
